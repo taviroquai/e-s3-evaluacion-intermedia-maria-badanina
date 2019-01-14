0d8df5cd5da33fd5e73f391460abda6cfc376027
maria-badanina <maria.badanina.s@gmail.com>
2019-01-14 15:45:24 +0100

    Create docs folder

---
+++ docs/precache-manifest.d441e0018c7b01a794951b32300fca9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5165156abcaeabdbb42f",
+    "url": "/e-s3-evaluacion-intermedia-maria-badanina/static/js/runtime~main.5165156a.js"
+  },
+  {
+    "revision": "da4e18d1f3aef9f5b9b3",
+    "url": "/e-s3-evaluacion-intermedia-maria-badanina/static/js/main.da4e18d1.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/e-s3-evaluacion-intermedia-maria-badanina/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "da4e18d1f3aef9f5b9b3",
+    "url": "/e-s3-evaluacion-intermedia-maria-badanina/static/css/main.91662bb6.chunk.css"
+  },
+  {
+    "revision": "2226241b99cfcce7e00503536cedbaa3",
+    "url": "/e-s3-evaluacion-intermedia-maria-badanina/index.html"
+  }
+];
